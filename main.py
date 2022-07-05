50b620e6074d8fb23bfbd69de8199256f5850793
KrazyCo <conorstrickland@hotmail.com>
2022-07-05 10:58:54 +0100

    basic search

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+from private import key
+import requests
+import pandas as pd
+
+url = "https://api.companieshouse.gov.uk/search/companies?items_per_page=100&q={}"
+
+query = "FM Global UK"
+
+query = query.replace(" ","+")
+query = query.replace("&","%26")
+
+result = requests.get(url.format(query), auth=(str(key),''))
+
+# print(result.json())
+# print(f"{result.json()['total_results'] = }")
+
+output = pd.json_normalize(result.json(), "items")
+# print(output)
+outputcsv = output[["title", "company_number", "company_status", "description", "address_snippet", "company_type", "address.postal_code"]]
+# print(outputcsv)
+
+saved = False
+while not saved:
+    try:
+        outputcsv.to_csv("search.csv")
+    except:
+        print("Cannot save - close excel")
+        time.sleep(0.5)
+    else:
+        saved = True
+        print("Saved")
